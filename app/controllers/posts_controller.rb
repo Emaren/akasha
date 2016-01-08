6eb275a4cea7269ff29840aecb388b7f229e6180
Tony Blum <tonyblum@me.com>
2016-01-08 08:27:58 -0800

    Add comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,13 +24,13 @@ class PostsController < ApplicationController
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
 
     if @post.update post_params
-      redirect_to root_path
+      redirect_to @post
     else
       render 'edit'
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
 
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true
   validates :body, presence: true
 
